3671fe556bcc6b115dee4b2a80d9f77a1faf0c87
yuka <u1_qsh1tq_way@yahoo.co.jp>
2019-02-16 06:42:41 +0000

    follow,unfollow

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-before_action :require_user_logged_in, only: [:index, :show]
+before_action :require_user_logged_in, only: [:index, :show, :followings, :followers]
 
   def index
     @users = User.all.page(params[:page])
@@ -26,8 +26,21 @@ before_action :require_user_logged_in, only: [:index, :show]
       render:new
     end
   end
+  
+  def followings
+    @user = User.find(params[:id])
+    @followings = @user.followings.page(params[:page])
+    counts(@user)
+  end
+  
+  def followers
+    @user = User.find(params[:id])
+    @followers = @user.followers.page(params[:page])
+    counts(@user)
+  end
+  
+  
 end
-
 
   private
   
